3c6777ae5313fef2ab16a045006136a900d46508
choha0416 <chohayun0416@gmail.com>
2024-11-01 10:48:19 +0900

    Add files via upload

---
+++ inputoutput/build/classes/pro/test/Number.java
@@ -0,0 +1,50 @@
+package pro.test;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Number
+ */
+@WebServlet("/Number")
+public class Number extends HttpServlet {
+
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Number() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String number1=request.getParameter("number1");
+		String number2=request.getParameter("number2");
+		String range=request.getParameter("range");
+		request.setAttribute("number1", number1);
+		request.setAttribute("number2", number2);
+		request.setAttribute("range", range);
+		RequestDispatcher dispatch = request.getRequestDispatcher("number.jsp?command=변경사항이 있으면 연락 부탁드립니다");
+		dispatch.forward(request, response);
+	}
+
+}
